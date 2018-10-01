bb398620251f5fc8ea46f2f08641900387702e21
Felix Sawicki <fmsawicki@gmail.con>
2018-10-01 11:43:27 -0400

    created models

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
+const Schema = mongoose.schema;
 app.listen(4000, (req, res) => {
   console.log("ACAB");
 });
